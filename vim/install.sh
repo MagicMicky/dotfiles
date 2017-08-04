a3d5b73f3076eaad54e0f9c0d4af136e28bcbd1c
MagicMicky <micky0304@gmail.com>
2017-08-04 18:51:54 +0200

    fix typo

--- vim/install.sh
+++ vim/install.sh
@@ -7,12 +7,12 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim-airline
 git clone https://github.com/vim-airline/vim-airline-themes ~/.vim-airline-themes
 mkdir ~/.vim ~/.vim/autoload ~/.vim/colors ~/.vim/doc ~/.vim/plugin
 ln -s ~/.vim-airline/autoload/airline ~/.vim/autoload/airline
-ln -s ~/.vim-airline/autoload/airline.vim ~/.vim/auload/airline.vim
+ln -s ~/.vim-airline/autoload/airline.vim ~/.vim/autoload/airline.vim
 
 ln -s ~/.vim-airline/doc/airline.txt ~/.vim/doc/airline.txt
 ln -s ~/.vim-airline/doc/airline-themes.txt ~/.vim/doc/airline-themes.txt
 
-ln -s ~/.vim-airline/plugin/airline.vim ~/.vim/plugins/airline.vim
+ln -s ~/.vim-airline/plugin/airline.vim ~/.vim/plugin/airline.vim
 ln -s ~/.vim-airline/plugin/airline-themes.vim ~/.vim/plugin/airline-themes.vim
 
 cp ./vim/autoload/airline/themes/* ~/.vim/autoload/airline/themes
